79a8ba4740f0adca2f8b78a802ef738429804546
Eugenio Lazaro <eugeniolazaro.elm@gmai>
2026-04-13 22:33:30 +0200

    reservas coche hotel

---
+++ reserva/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import React,{useState} from "react"
+import ComponenteMultiple from './components/componenteMultiple';
+
+function App() {
+  const [dias, setDias]=useState(0);
+  const [opcion, setOpcion]=useState("hotel");  
+
+  const selectorHotelCoche=(e)=>{
+    setOpcion(e.target.value);
+  };
+
+  return <div>
+    <select onChange={selectorHotelCoche} value={opcion}>
+      <option value="hotel">Hotel</option>
+      <option value="coche">Coche</option>
+    </select>
+
+    <input onChange={e=>setDias(e.target.value)} type="text" placeholder="Nº dias"/>
+    
+    {opcion==="hotel" && <ComponenteMultiple opcion={1} dias={dias}/>}
+    {opcion==="coche" && <ComponenteMultiple opcion={2} dias={dias}/>}
+
+
+  </div>
+}
+
+export default App;
